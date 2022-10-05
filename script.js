8b4a0308459e4e50e9e5e00837093e9feb91f1df
gnahc2305 <achangmurillo@gmail.com>
2022-10-05 17:17:29 -0500

    create files

---
+++ script.js
@@ -0,0 +1,34 @@
+let myLibrary = [
+    {
+        book: 'The Hobbit',
+        author: 'J.R.R Tolkien',
+        length: '295 pages',
+        status: 'not read yet',
+    },
+    {
+
+    }
+];
+
+function addBookToLibrary() {
+
+}
+
+function Book() {
+
+}
+
+// function Book(book, author, length, status) {
+//     this.book = book
+//     this.author = author
+//     this.length = length
+//     this.status = status
+// }
+
+// Book.prototype.info = function() {
+//     return `${this.book} by ${this.author}, ${this.length}, ${this.status}`
+// }
+  
+// const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', '295 pages', 'not read yet' )
+
+// console.log(theHobbit.info())
