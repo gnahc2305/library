fbc5c4855ff7426a7194c5c7d46c0de95376ea67
gnahc2305 <achangmurillo@gmail.com>
2022-10-05 18:04:13 -0500

    create html layout

--- script.js
+++ script.js
@@ -1,34 +1,42 @@
 let myLibrary = [
-    {
-        book: 'The Hobbit',
-        author: 'J.R.R Tolkien',
-        length: '295 pages',
-        status: 'not read yet',
-    },
-    {
+    // {
+    //     book: 'The Hobbit',
+    //     author: 'J.R.R Tolkien',
+    //     length: '295 pages',
+    //     status: 'not read yet',
+    // },
+    // {
 
-    }
+    // }
 ];
 
 function addBookToLibrary() {
+    bookName = prompt('Name of the book:');
+    bookAuthor = prompt('Author of the book:');
+    bookLength = prompt('Length of the book:');
+    bookStatus = prompt('Have you read the book?');
 
+    const userBook = new Book(bookName, bookAuthor, bookLength, bookStatus);
+    myLibrary.push(userBook);
+    console.table(myLibrary);
 }
 
-function Book() {
-
+function Book(book, author, length, status) {
+    this.book = book
+    this.author = author
+    this.length = length
+    this.status = status
 }
 
-// function Book(book, author, length, status) {
-//     this.book = book
-//     this.author = author
-//     this.length = length
-//     this.status = status
-// }
-
-// Book.prototype.info = function() {
-//     return `${this.book} by ${this.author}, ${this.length}, ${this.status}`
-// }
+Book.prototype.info = function() {
+    return `${this.book} by ${this.author}, ${this.length}, ${this.status}`
+}
   
-// const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', '295 pages', 'not read yet' )
+const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', '295 pages', 'not read yet' )
+
+
+// myLibrary.push(theHobbit);
+
+// console.table(myLibrary);
 
 // console.log(theHobbit.info())
