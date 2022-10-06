ff6f7a2a0de660516c6cbdf0333bba8d7ca7298d
gnahc2305 <achangmurillo@gmail.com>
2022-10-05 19:00:39 -0500

    create modal

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+let modal = document.getElementById('myModal');
+let btn = document.getElementById("myBtn");
+let span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// open and close modal
+
+
 let myLibrary = [
     // {
     //     book: 'The Hobbit',
